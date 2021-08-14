12edffa7d0d016974e0055944b799d02b371448a
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-08-14 14:05:24 +0300

    Main.java added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Price = 15600;
+        int priceForOneMili = 20;
+        int totalMili = Price / priceForOneMili;
+        System.out.println("Полученные Вами мили: " + totalMili);
+    }
+}
