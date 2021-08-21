c1ebedb52f6d43f5650ca8a4a28294ab2f7f567b
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-08-21 14:20:07 +0300

    calculate changed

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,6 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price, int priceForOneMili) {
-        int totalMili = price / priceForOneMili;
+    public int calculate(int cost) {
+        int priceForOneMili = 20;
+        int totalMili = cost / priceForOneMili;
         return totalMili;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
         public class Main {
             public static void main(String[] args) {
                 BonusMilesService service = new BonusMilesService();
-                int finelMiles = service.calculate(15600, 20);
+                int finelMiles = service.calculate(15600);
                 System.out.println(finelMiles);
             }
         }
